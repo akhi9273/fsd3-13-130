f41439c30f71238e1f69818b10c8558ab8560930
akhi9273 <r0764168@gmail.com>
2026-09-15 16:31:20 +0530

    commit

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -2,38 +2,37 @@ import http from "http";
 
 const server = http.createServer((req, res) => {
 
-    // GET Request
+    
     if (req.url === "/" && req.method === "GET") {
         res.statusCode = 200;
         res.end("GET Request");
     }
 
-    // POST Request
+    
     else if (req.url === "/" && req.method === "POST") {
         res.statusCode = 200;
         res.end("POST Request");
     }
 
-    // PUT Request
+    
     else if (req.url === "/" && req.method === "PUT") {
         res.statusCode = 200;
         res.end("PUT Request");
     }
-
-    // DELETE Request
+ 
     else if (req.url === "/" && req.method === "DELETE") {
         res.statusCode = 200;
         res.end("DELETE Request");
     }
 
-    // If route/method does not match
+
     else {
         res.statusCode = 404;
         res.end("Page Not Found");
     }
 });
 
-// Start server
+
 server.listen(5000, () => {
     console.log("prg6 is running on port 5000");
 });
