016b1f0e97abf3b0336418b963c885587d25cf56
akhi9273 <r0764168@gmail.com>
2026-09-23 12:22:55 +0530

    express start

--- lab3/products.js
+++ lab3/products.js
@@ -1,46 +1,45 @@
-const products=[
-    {id:1,name:'marker',qty:100,price:15},
-    {id:2,name:'duster',qty:50,price:10},
-]
-let nextId=3;
-export const getAllProducts=() =>{
-    return products;
+const products = [
+  { id: 1, name: "marker", qty: 100, price: 15 },
+  { id: 2, name: "duster", qty: 50, price: 10 },
+];
 
-}
- export const addProducts=() => {
-       item.id= nextId;
-       nextId++;
-       products.push(item);
-       return item;
- };
-export const  deleteProducts=(pip) =>{
-    const item = products.findIndex((prd)=>prd.id===pid);
-    if(item ==-1)
-        return false;
-    products.splice(item,1);
-    console.log("products remaining:",products);
-    return true;
+let nextId = 3;
 
-
+export const getAllProducts = () => {
+  return products;
 };
-//craete  as function a to update  products even pid call this function in to prg6.js  and verify its working by  echoapi
-export  const UpdateProducts=(pid)=>{
-    const item=products.findIndex((pid)=>prd.id===pid)
-        if(item==-1){
-            return false;
 
-        }
-        updateItem.id=pid;
-        products(item)=updateItem;
-        return updateItem;
-        
+export const addProduct = (item) => {
+  item.id = nextId;
+  nextId++;
+  products.push(item);
+  return item;
+};
 
-    };
-    export const getProductsByI=(pid) =>{
-        const index = products.findIndex((prd)=>prd.id===pid);
-        if(index==-1){
-            return false;
-        }
-        return products[index];
-    };
+export const deleteProduct = (pid) => {
+  const item = products.findIndex((prd) => prd.id === pid);
+  if (item == -1) return false;
+  products.splice(item, 1);
+  console.log("products remaining:", products);
+  return true;
+};
 
+export const updateProduct = (pid, updateItem) => {
+  const index = products.findIndex((prd) => prd.id === pid);
+
+  if (index == -1) {
+    return false;
+  }
+  updateItem.id = pid;
+  products[index] = updateItem;
+  return updateItem;
+};
+
+export const getProductById = (pid) => {
+  const index = products.findIndex((prd) => prd.id === pid);
+
+  if (index == -1) {
+    return false;
+  }
+  return products[index];
+};
